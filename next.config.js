9c927c1a46c8f3004a1422b5810e6109de845633
Francisco Moretti <franciscoemoretti@gmail.com>
2024-01-14 12:01:34 +0000

    config files and shadcn/ui

--- next.config.js
+++ next.config.js
@@ -2,9 +2,19 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import { withContentlayer } from "next-contentlayer"
+
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    reactStrictMode: true,
+    images: {
+      domains: ["avatars.githubusercontent.com"],
+    },
+    experimental: {
+      serverComponentsExternalPackages: ["@prisma/client"],
+    },
+};
 
-export default config;
+export default withContentlayer(config);
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
-import { ClassValue, clsx } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { env } from "@/env.js"
 
-import { env } from "@/env.mjs"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
